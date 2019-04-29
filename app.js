f322ca7ffd7c9c0be325c642ae2083a9973ef959
gucciassistant <49936841+gucciassistant@users.noreply.github.com>
2019-04-29 16:37:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.post('/webhook/', (req, res) => {
                                     //console.log('Code--->', result.code);
                                     console.log(cartResult.basketId+' '+result.token+" "+result.token);
 					  console.log(cartResult.basketId);
+					console.log(cartResult.name);
                                     text = "Yes, there is currently a promotion - they are at 200 swiss francs until the end of the month and are available at your usual Cap Sports Style store. Same color as current one";
                                     messageData = {
                                         speech: text,
