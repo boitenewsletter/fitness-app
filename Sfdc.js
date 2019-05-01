1c2f2f41961c0aa9370415aac7043f3af9da83c3
gucciassistant <49936841+gucciassistant@users.noreply.github.com>
2019-05-01 22:53:37 +0530

    Update Sfdc.js

--- Sfdc.js
+++ Sfdc.js
@@ -141,8 +141,9 @@ var getUserDetails = (authToken, callback) => {
 };
 
 
-var updatecase = (authToken, callback) => {
+var updatecase = (authToken, test, callback) => {
      console.log(authToken);
+    console.log(test);
     console.log('Placing order API hit');
     request({
         url: `https://kering--SbxDamDev.cs102.my.salesforce.com/services/data/v44.0/sobjects/Case`,
